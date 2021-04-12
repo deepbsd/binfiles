6b3c5ae74d9567c8545822f03e64f3d37a4ecc9c
David S. Jackson <deepbsd@yahoo.com>
2021-04-12 13:25:19 -0400

    using the example of an infobox here

--- whipsample.sh
+++ whipsample.sh
@@ -12,9 +12,14 @@ greeter(){
     whiptail --msgbox --title "Greetings" "$message" 10 30
 }
 
+callshowinfo(){
+    showinfo 
+    sleep 10 
+}
+
 showinfo(){
-    info=$1
-    whiptail --title "Here's an Infobox" --infobox "Here's an infobox example" 10 30 
+    message="An infobox disappears immediately when the calling shell script is complete.\n\nYou use it to show a panel until a process completes.\n\nOtherwise, you can use a --msgbox \n\nIn this example, we're waiting for 10 seconds."
+    TERM=ansi whiptail --title "Here's an Infobox" --infobox "$message" 20 70 
 }
 
 choosecolor(){
@@ -49,13 +54,13 @@ mainmenu(){
         whiptail --title "What shall we do?" --menu "Your choices" 16 80 9 \
             "1"    "inputbox sample"  \
             "2"    "menu sample"      \
-            "3"    "infobox sample"      \
+            "3"    "infobox sample"   \
             "X"    "Exit"  3>&2 2>&1 1>&3
         )
         case $choice in 
             "1")    getname ;;
             "2")    choosecolor ;;
-            "3")    showinfo ;;
+            "3")    callshowinfo ;;
             "X")    echo "Bye for now!" && exit 0 ;;
             "*")    echo "Please choose valid option." ;;
         esac
