8ea15b05943fd668a61f7ce6f92adc33ae1412b9
David S. Jackson <deepbsd@yahoo.com>
2021-04-12 13:10:31 -0400

    playing with whipsample.sh

--- whipsample.sh
+++ whipsample.sh
@@ -3,13 +3,18 @@
 showcolor(){
     color=$1
     message=$(echo "You chose $color") 
-     whiptail --msgbox --title "Your color choice" "$message" 10 30
+    whiptail --msgbox --title "Your color choice" "$message" 10 30
 }
 
 greeter(){
     name=$1
     message=$(echo "Greetings $name!")
     whiptail --msgbox --title "Greetings" "$message" 10 30
+}
+
+showinfo(){
+    info=$1
+    whiptail --title "Here's an Infobox" --infobox "Here's an infobox example" 10 30 
 }
 
 choosecolor(){
@@ -20,14 +25,14 @@ choosecolor(){
             "2)"    "blue"  \
             "3)"    "green"  \
             "4)"    "white"  \
-            "5)"    "exit"  3>&2 2>&1 1>&3
+            "5)"    "main menu"  3>&2 2>&1 1>&3
         )
         case $choice in
             "1)" ) showcolor "red" ;;
             "2)" ) showcolor "blue" ;;
             "3)" ) showcolor "green" ;;
             "4)" ) showcolor "white" ;;
-            "5)" ) echo "Bye for now!" && exit 0 ;;
+            "5)" ) mainmenu ;;
         esac
     done
 }
@@ -37,5 +42,24 @@ getname(){
     greeter $name
 }
 
-#choosecolor
-getname
+mainmenu(){
+
+    while true; do
+        choice=$(
+        whiptail --title "What shall we do?" --menu "Your choices" 16 80 9 \
+            "1"    "inputbox sample"  \
+            "2"    "menu sample"      \
+            "3"    "infobox sample"      \
+            "X"    "Exit"  3>&2 2>&1 1>&3
+        )
+        case $choice in 
+            "1")    getname ;;
+            "2")    choosecolor ;;
+            "3")    showinfo ;;
+            "X")    echo "Bye for now!" && exit 0 ;;
+            "*")    echo "Please choose valid option." ;;
+        esac
+    done
+}
+
+mainmenu
