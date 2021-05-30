f42c9c1aa18ea7d717c31a7343f09a3b62d427eb
David S. Jackson <deepbsd@yahoo.com>
2021-05-30 18:22:37 -0400

    working on a whiptail menu size problem; menu is too long, how to handle that?

--- find_keymaps.sh
+++ find_keymaps.sh
@@ -1,11 +1,35 @@
 #!/usr/bin/env bash
 
-keymaps=()
 keymaps=$(find /usr/share/kbd/keymaps/ -type f -printf "%f\n" | sort -V)
 
+keymaps=( $keymaps )
+
+status=""
 options=()
 
-for kmap in ${keymaps[@]} ; do
-    echo ${kmap} | sed 's/.map.gz//g'
-    options+=( "${kmap%.*}" )
+echo "keymaps: ${#keymaps[@]}"
+
+for file in "${keymaps[@]}"; do
+    if [[ "${file%%.*}" == 'us' ]]; then
+        status="ON"
+    else
+        status="OFF"
+    fi
+
+    newfile=$( echo $file | sed 's/.map.gz//g' )
+    #options+=( "$newfile \" \"  $status"  )
+    options+=( $(printf "%s\t%s\n" $newfile '.....')  )
 done
+
+#echo "number: ${#options[@]}"
+
+#for line in "${options[@]}"; do
+#    echo $line
+#done
+
+options=( "${options[@]:0:30}" )
+
+selection=$(eval `resize`; whiptail --title "Choose Keyboard Map" --menu "Default is us keymap" \
+    $((8+${#options[@]})) 80 $(("${#options[@]}")) "${options[@]}" 3>&1 1>&2 2>&3 )
+
+echo "Your keymap is ${selection:-'howdie folks!'}"
