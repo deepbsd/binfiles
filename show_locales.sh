da01ab906e0b9ccfa0bcadb111a4c0d4d1931d81
David S. Jackson <deepbsd@yahoo.com>
2021-06-08 15:02:54 -0400

    this version works better for locales

--- show_locales.sh
+++ show_locales.sh
@@ -4,12 +4,15 @@
 
 locales=()
 
-if $(whiptail --backtitle "KEEP LOCALE?" --title "Want to keep LOCALE as en_US.UTF-8?" --yesno --yes-button "Keep en_US.UTF-8" --no-button "Change LOCALE" ) ; then
+if $(whiptail --backtitle "KEEP LOCALE?" --title "Want to keep LOCALE as en_US.UTF-8?" --yesno "Yes or No"\
+    --yes-button "Keep en_US.UTF-8" --no-button "Change LOCALE" 20 80 3>&1 1>&2 2>&3 ) ; then
+
     LOCALE=${LOCALE:="en_US.UTF-8"}
 else
     # Here's the array of available locales:
-    for locale in $(egrep '^#?[a-z]{2}_*' /etc/locale.gen | sed 's/^#//g'); do
-        locales+=( $(printf "%s\t\t%s\n" "$locale" "=======")  )
+    for locale in $(egrep '^#?[a-z]{2}_*' /etc/locale.gen | awk '{print $1}' | sed 's/^#//g'); do
+        locale=$( echo $locale | sed 's/ /./g' )
+        locales+=( $(printf "%s\t\t%s\n" "$locale" "locale")  )
     done
 
     # Come up with a whiptail selection menu of all available locales on system
@@ -18,9 +21,9 @@ else
         --menu "Default Locale is en_US.UTF-8" $LINES $COLUMNS $(( $LINES - 8 )) "${locales[@]}" 3>&1 1>&2 2>&3 )
 fi
 
-# doublecheck each line
-for line in "${locales[@]}"; do
-    echo $line
-done
+## doublecheck each line
+#for line in "${locales[@]}"; do
+#    echo $line
+#done
 
 echo ${LOCALE:="Have a nice day!"}
