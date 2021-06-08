c7bfe1d42325e1ddb23187fb7824cdcfd122898e
David S. Jackson <deepbsd@yahoo.com>
2021-06-08 00:00:42 -0400

    trying out show_locales idea

--- show_locales.sh
+++ show_locales.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
 
-LOCALES=()
+locales=()
 
 if $(whiptail --backtitle "KEEP LOCALE?" --title "Want to keep LOCALE as en_US.UTF-8?" \
-    --yesno --yes "Keep en_US.UTF-8" --no "Change LOCALE" ); then
+    --yesno --yes-button "Keep en_US.UTF-8" --no-button "Change LOCALE" ); then
     LOCALE=${LOCALE:="en_US.UTF-8"}
 else
     # Here's the array of available locales:
     for locale in $(egrep '^#?[a-z]{2}_*' /etc/locale.gen | sed 's/^#//g'); do
-        LOCALES+=( $(printf "%s\t\t%s\n" $locale "=======")  )
+        locales+=( $(printf "%s\t\t%s\n" $locale "=======")  )
     done
 
     # Come up with a whiptail selection menu of all available locales on system
-
+  
+    LOCALE=$(eval `resize`; whiptail --backtitle "CHOOSE LOCALE" --title "Choose Your Locale" \
+        --menu "Default Locale is en_US.UTF-8" $LINES $COLUMNS $(( $LINES - 8 )) "${locales[@]}" 3>&1 1>&2 2>&3 )
 fi
 
-echo "${#LOCALES[@]}"
+echo ${LOCALE:="Have a nice day!"}
