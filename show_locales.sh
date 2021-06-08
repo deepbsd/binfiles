8f3e0bb83fbb4bfdb0404f80f3c29ae756e99b9b
David S. Jackson <deepbsd@yahoo.com>
2021-06-08 00:11:13 -0400

    still working on getting contents into locales array properly

--- show_locales.sh
+++ show_locales.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 
+
 locales=()
 
-if $(whiptail --backtitle "KEEP LOCALE?" --title "Want to keep LOCALE as en_US.UTF-8?" \
-    --yesno --yes-button "Keep en_US.UTF-8" --no-button "Change LOCALE" ); then
+if $(whiptail --backtitle "KEEP LOCALE?" --title "Want to keep LOCALE as en_US.UTF-8?" --yesno --yes-button "Keep en_US.UTF-8" --no-button "Change LOCALE" ) ; then
     LOCALE=${LOCALE:="en_US.UTF-8"}
 else
     # Here's the array of available locales:
     for locale in $(egrep '^#?[a-z]{2}_*' /etc/locale.gen | sed 's/^#//g'); do
-        locales+=( $(printf "%s\t\t%s\n" $locale "=======")  )
+        locales+=( $(printf "%s\t\t%s\n" "$locale" "=======")  )
     done
 
     # Come up with a whiptail selection menu of all available locales on system
@@ -17,5 +17,10 @@ else
     LOCALE=$(eval `resize`; whiptail --backtitle "CHOOSE LOCALE" --title "Choose Your Locale" \
         --menu "Default Locale is en_US.UTF-8" $LINES $COLUMNS $(( $LINES - 8 )) "${locales[@]}" 3>&1 1>&2 2>&3 )
 fi
+
+# doublecheck each line
+for line in "${locales[@]}"; do
+    echo $line
+done
 
 echo ${LOCALE:="Have a nice day!"}
