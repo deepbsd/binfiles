626ec8a9e8da713d8d5907e358a982b3e39a1d90
David S. Jackson <deepbsd@yahoo.com>
2021-06-10 20:10:37 -0400

    working on timezone

--- set_timezeone.sh
+++ set_timezeone.sh
@@ -3,11 +3,11 @@
 timezones=()
 for timezone in $(timedatectl list-timezones); do
     #timezones+=( "$timezone" "$(basename $timezone)" )
-    timezones+=( "$timezone" "$(echo hey)" )
+    timezones+=( $(printf "%s\t\t%s\n" $timezone "$timezone timezone") )
 done
 
 backmessage="CHOOSE YOUR TIMEZONE"
 message="Please choose your timezone"
-timezone=$(eval `resize`; whiptail --backtitle $backmessage --title $message --menu "Here are your timezones:" $LINES $COLUMNS $(( LINES - 8 )) 3>&1 1>&2 2>&3)
+timezone=$(eval `resize`; whiptail --backtitle $backmessage --title $message --menu "Here are your timezones:" "${timezones[@]}" $LINES $COLUMNS $(( LINES - 8 )) 3>&1 1>&2 2>&3)
 
 echo $timezone
