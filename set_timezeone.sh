eb916f168b7ce2a425a0fdf861566aeafeadd3cc
David S. Jackson <deepbsd@yahoo.com>
2021-06-10 20:32:47 -0400

    updated set_timezone

--- set_timezeone.sh
+++ set_timezeone.sh
@@ -2,7 +2,7 @@
 
 timezones=()
 for timezone in $(timedatectl list-timezones); do
-    #timezones+=( "$timezone" "$(basename $timezone)" )
+
     timezones+=( $(printf "%s\t\t%s\n" $timezone 'timezone') )
 done
 
@@ -12,8 +12,5 @@ message="Please choose your timezone"
 timezone=$(eval `resize`; whiptail --backtitle "$backmessage" --title "$message" \
     --menu "Here are your timezones:" $LINES $COLUMNS $(( $LINES - 8 )) "${timezones[@]}"  3>&1 1>&2 2>&3)
 
-
-#timezone=$(eval `resize`; whiptail --backtitle "CHOOSE KEYMAP" --title "Choose Keyboard Map"  \
-#    --menu "Default is us keymap" $LINES $COLUMNS $(( $LINES - 8 )) "${options[@]}" 3>&1 1>&2 2>&3 )
 
 echo $timezone
