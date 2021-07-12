45c4b25a1c2ddef2728fac8c36455e13c3358ffe
David S. Jackson <deepbsd@yahoo.com>
2021-07-11 21:36:35 -0400

    creating partitions with parted...

--- cryptsetup.sh
+++ cryptsetup.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-# Let's set up the steps for automating a crypt setup for lvm partitioning with luks!
+# Let's set up the steps for automating a crypt setup for lvm on uefi, partitioning with luks!
+# we're using parted for creating partitions
+
+DRIVE=/dev/sda
+
+parted -s "$DRIVE" mklabel gpt
+
+parted -s "$DRIVE" unit mib mkpart primary 1 512 
+
+parted -s "$DRIVE" unit mib mkpart primary 2 100%
+
+parted -s "$DRIVE" set 2 lvm on
+
+
