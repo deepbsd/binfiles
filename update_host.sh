7f6b0513a315c4aa582a033e939b485bb57e6fef
David S. Jackson <deepbsd@yahoo.com>
2025-05-29 16:19:40 -0400

    updated update_host script

--- update_host.sh
+++ update_host.sh
@@ -16,6 +16,7 @@ locked_hosts=()
 hosts=( $(cat $hosts_file) )
 arch_hosts=( boomer scee daisy speedy juno peach dixie molly blub milo pumpkin )
 deb_hosts=( roscoe omalley )
+down_hosts=()
 
 
 
@@ -31,23 +32,35 @@ identify(){    # Get the users credentials
     if pgrep ssh-agent ; then
         echo ssh-agent already running...
     else
-        eval $(ssh-agent)
+        eval "$(ssh-agent -s)"
     fi
-    ssh-add
+    ssh-add ~/.ssh/id_rsa
+
     echo "$USER's identify added...."
 }
 
+host_is_up(){     # pass the hostname to check as $1
+# check and make sure the host is up on the localnet
+
+    ( ping -c3 $1 &>/dev/null ) && return 0
+    down_hosts+=( $1 )
+    echo -e "\n=====> HOST: $1 is Down <======\n"
+    return 1
+
+}
 
 update_host(){     # Run the actual update on each host in the file
 
     for h in "${hosts[@]}"; do
 
-        echo "=======> HOST: $h  <======="
+        host_is_up "$h" || continue  # skip hosts that are down
+
+        echo -e "\n=======> HOST: $h  <=======\n"
 
         if [ ! `cat /etc/hostname` == "$h" ]; then
 ssh -tt $USER@$h.lan  << EOF
 [[ -f "$lock_file" ]] && locked_hosts+=( "$h.lan" )
-[[ "${arch_hosts[@]}" =~ "$h" ]] && echo "$passwd" | sudo -S pacman --noconfirm -Syyu
+[[ "${arch_hosts[@]}" =~ "$h" ]] && echo "$passwd" | sudo -S pacman --noconfirm -Syyu 
 [[ "${deb_hosts[@]}" =~ "$h" ]] && echo "$passwd" | sudo -S apt update && sudo -S apt dist-upgrade -y
 exit
 EOF
@@ -64,7 +77,7 @@ print_locked(){
     if [ ${#locked_hosts[@]} -eq 0 ] ; then
         echo "=== No Locked Hosts ==="
     else
-        echo "=== LOCKED HOSTS ==="
+        echo -e "\n=== LOCKED HOSTS ===\n"
         echo "${locked_hosts[@]}"
     fi
 }
