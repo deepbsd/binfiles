09c23781bd2f84c21c756b75e68f228b4f3681e5
David S. Jackson <deepbsd@yahoo.com>
2025-07-03 22:02:12 -0400

    moved tmp file to ~/tmp/.

--- max_heart_tanaka.sh
+++ max_heart_tanaka.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+tmp_file=$HOME/tmp/output_text
 age=0
 factor=0
 max_hr=0
@@ -42,7 +43,7 @@ show_zones(){
     Sixtypct=$(echo "scale=2; $max_hr*0.6" | bc)
     Fiftypct=$(echo "scale=2; $max_hr*0.5" | bc)
 
-    echo -e "\t\t$subtitle\n\n\n\t\tZone 5: $Ninetypct - $max_hr \n\t\tZone 4: $Eightypct - $Ninetypct \n\t\tZone 3: $Seventypct - $Eightypct \n\t\tZone 2: $Sixtypct - $Seventypct \n\t\tZone 1: $Fiftypct - $Sixtypct\n" > output_text
+    echo -e "\t\t$subtitle\n\n\n\t\tZone 5: $Ninetypct - $max_hr \n\t\tZone 4: $Eightypct - $Ninetypct \n\t\tZone 3: $Seventypct - $Eightypct \n\t\tZone 2: $Sixtypct - $Seventypct \n\t\tZone 1: $Fiftypct - $Sixtypct\n" > $tmp_file
     TERM=ansi whiptail --backtitle "Showing Your HR Training Zones" --title "Your Training Zones" --textbox output_text 20 50 
 
 
